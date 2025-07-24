f2e8eb3858a2b774be0f449f509fa7a67be20529
derickshanana <derickshanana50@gmail.com>
2025-07-24 20:11:29 +0200

    changed

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,9 +1,9 @@
 public class FeatureCollection
-{
+
     // TODO Problem 5 - ADD YOUR CODE HERE
     // Create additional classes as necessary
-}
-public class FeatureCollection
+
+
 {
     // TODO Problem 5 - ADD YOUR CODE HERE
     // Create additional classes as necessary
